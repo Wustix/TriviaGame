2fac157271772edf6445369fa1be71470c4aa300
unknown <wroehrman@yahoo.com>
2018-06-10 13:21:18 -0500

    update to trivia js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,112 +1,45 @@
-var triviaQuestions = [
-    {
-        question: "What makes time-travel possible in the movie Back to the Future?",
-        answers: {
-            a: "Faux Capacitor",
-            b: "Flux Capacitor",
-            c: "Phlem Capacitor",
-            d: "Full Capacitor"
-        },
-        correctAnswer: "c"
-    },
-    {
-        question: "Who is the blonde hero who will &#34;save everyone of us&#34;",
-        answers: {
-            a: "Hash Smokem",
-            b: "Mash Floorman",
-            c: "Flash Gordon",
-            d: "Dash Gordmans"
-        },
-        correctAnswer: "c"
-    },
-    {
-        question: "In Star Wars lore, what powers a lightsaber and gives it a distintive color?",
-        answers: {
-            a: "D Battery",
-            b: "Bug Zapper",
-            c: "Red Bull",
-            d: "Kyber Crystal"
-        },
-        correctAnswer: "d"
-    },
-    {
-        question: "What was the name of the character played by John Candy in Spaceballs?",
-        answers: {
-            a: "Darth",
-            b: "Barf",
-            c: "Dark Helmet",
-            d: "Sloth"
-        },
-        correctAnswer: "b"
-    },
-    {
-        question: "Doctor Who travels in a blue police box called a TARDIS which stands for what?",
-        answers: {
-            a: "Torrid Andromeda Romulan Ducati Integer Specific",
-            b: "Tracking Around Regions Declared In Space",
-            c: "Tacos Are Really Dumb I Say",
-            d: "Time And Relative Dimensions In Space"
-        },
-        correctAnswer: "d"
-    },
-    {
-        question: "Cylons are the bad guys in what television series?",
-        answers: {
-            a: "The Million Dollar Man",
-            b: "Battlestar Galactica",
-            c: "Mork and Mindy",
-            d: "Battleship Uranus"
-        },
-        correctAnswer: "b"
-    },
-    {
-        question: "In Ray Bradburys classic novel, paper bursts into flames at what&#34;Fahrenheit ________&#34; temperature?",
-        answers: {  
-            a: "451&#186;",
-            b: "215&#186;",
-            c: "100&#186;",
-            d: "1000&#186;"
-        },
-        correctAnswer: "a"
-    },
-    {
-        question: "In which 1990 sci-fi movie did we see Kevin Bacon being terrorized by gigantic, hungry worms?",
-        answers: {
-            a: "Slither",
-            b: "Feast",
-            c: "What Waits Below",
-            d: "Tremors"
-        },
-        correctAnswer: "d"
-    },
-    {
-        question: "In the classic sci-fi film &#34;2001-A Space Odyssey,&#34; what did the computer HAL find out was to happen by reading Bowman and Pool&#39s lips?",
-        answers: {
-            a: "The were abandoning the ship",
-            b: "He was being disconnected",
-            c: "They were returning to Earth",
-            d: "Ship was going to explode"
-        },
-        correctAnswer: "b"
-    },
-    {
-        question: "Ellen Riplay had to fight off what terrifying creature in the &#34;Alien&#34; moive films?",
-        answers: {
-            a: "X-men",
-            b: "Tribble",
-            c: "Xenomorph",
-            d: "Jack Russell Terrier"
-        },
-        correctAnswer: "c"
-    },
-];
-
-
-
-
-
 $(document).ready(function () {
 
-    
+    var triviaQuestions = ["What makes time-travel possible in the movie Back to the Future?",
+        "Who is the blonde hero who will &#34;save everyone of us&#34;",
+        "In Star Wars lore, what powers a lightsaber and gives it a distintive color?",
+        "What was the name of the character played by John Candy in Spaceballs?",
+        "Doctor Who travels in a blue police box called a TARDIS which stands for what?",
+        "Cylons are the bad guys in what television series?",
+        "In Ray Bradburys classic novel, paper bursts into flames at what&#34;Fahrenheit ________&#34; temperature?",
+        "In which 1990 sci-fi movie did we see Kevin Bacon being terrorized by gigantic, hungry worms?",
+        "In the classic sci-fi film &#34;2001-A Space Odyssey,&#34; what did the computer HAL find out was to happen by reading Bowman and Pool&#39s lips?",
+        "Ellen Riplay had to fight off what terrifying creature in the &#34;Alien&#34; moive films?",
+    ];
+
+    var triviaAnswers = [["Faux Capacitor", "Flux Capacitor", "Phlem Capacitor", "Full Capacitor"]
+    ["Hash Smokem", "Mash Floorman", "Flash Gordon", "Dash Gordmans"]
+    ["D Battery", "Bug Zapper", "Red Bull", "Kyber Crystal"]
+    ["Darth", "Barf", "Dark Helmet", "Sloth"]
+    ["Torrid Andromeda Romulan Ducati Integer Specific", "Tracking Around Regions Declared In Space", "Tacos Are Really Dumb In Space", "Time And Relative Dimensions In Space"]
+    ["The Million Dollar Man", "Battlestar Galactica", "Mork and Mindy", "Battleship Uranus"]
+    ["451&#186;", "215&#186;", "100&#186;", "1000&#186;"]
+    ["Slither", "Feast", "What Waits Below", "Tremors"]
+    ["The were abandoning the ship", "He was being disconnected", "They were returning to Earth", "Ship was going to explode"]
+    ["X-men", "Tribble", "Xenomorph", "Jack Russell Terrier"]
+    ]
+
+
+
+    function goQuiz() {
+
+    }
+
+    function seeResults() {
+
+    }
+
+
+
+
+
+
+
+
 
 });
